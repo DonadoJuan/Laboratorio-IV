207e5b19b9bc1ca4f229919607a9d1b23a617563
juandonado <donado.juan@gmail.com>
2016-09-28 22:37:11 -0300

    juegos y login

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -8,6 +8,7 @@ $authProvider.tokenName = "mitoken"
 $authProvider.tokenPrefix = "proyecto"
 $authProvider.authHeader = "data"
 
+
   $stateProvider
 
   	// pagina principal
@@ -59,6 +60,7 @@ $authProvider.authHeader = "data"
                 views: {
                     'contenido': {
                         templateUrl: 'vistas/juegoJuego1.html',
+                        controller: 'juegoUnoCtrl'
                     }
                 }
             })      		
@@ -68,6 +70,7 @@ $authProvider.authHeader = "data"
                 views: {
                     'contenido': {
                         templateUrl: 'vistas/juegoJuego2.html',
+                        controller: 'juegoDosCtrl'
                     }
                 }
             })
@@ -81,28 +84,141 @@ $authProvider.authHeader = "data"
 
 })
 
+.controller('juegoUnoCtrl',function($scope,$http,$window){
+
+    $scope.gameover = true;
+
+    $scope.iniciarJuego = function(){
+
+        $scope.gameover = false;
+        $scope.intentos = 0;
+        $scope.numeroLoco = Math.floor((Math.random()*2)+1);
+    }
+
+    $scope.check = function(){
+
+        if($scope.numeroLoco == $scope.numIngresado){
+            $scope.intentos++;
+            $window.alert("Ganaste maquinola! disfruta tu premio");
+            $scope.gameover = true;
+        }
+
+        else{
+
+            $scope.intentos++;
+            if($scope.intentos > 4){
+                $window.alert("Perdiste pequeño rufian, atenete a las consecuencias...");
+                $scope.gameover = true;
+            }
+        }
+
+        $scope.numIngresado = "";
+    }
+
+})
+
+.controller('juegoDosCtrl',function($scope,$http,$window){
+
+    $scope.iniciarJuego = function(){
+
+        $scope.gameover = false;
+        $scope.ganaste = false;
+
+        $scope.uno = "regalo.png";
+        $scope.dos = "regalo.png";
+        $scope.tres = "regalo.png";
+        $scope.cuatro = "regalo.png";
+        $scope.cinco = "regalo.png";
+        $scope.seis = "regalo.png";
+
+        $scope.CajaGanadora = Math.floor((Math.random()*6)+1);
+    }
+
+    $scope.abrirRegalo = function(numeroRegalo){
+
+        $scope.gameover = true;
+
+        if($scope.CajaGanadora == numeroRegalo){
+            $window.alert("Ganaste tigre de los alpes suizos!");
+            $scope.ganaste = true;        
+        }
+
+        else{
+            $window.alert("Perdiste, ahora sos amigo de peluchin :(");
+        }
+
+        if(!$scope.ganaste){
+
+            switch(numeroRegalo){
+                case '1': $scope.uno = "FLAVIO.jpg";
+                break;
+                case '2': $scope.dos = "FLAVIO.jpg";
+                break;
+                case '3': $scope.tres = "FLAVIO.jpg";
+                break;
+                case '4': $scope.cuatro = "FLAVIO.jpg";
+                break;
+                case '5': $scope.cinco = "FLAVIO.jpg";
+                break;
+                case '6': $scope.seis = "FLAVIO.jpg";
+            }
+        }
+
+
+    else{
+
+        switch(numeroRegalo){
+            case '1': $scope.uno = "ganador.jpg";
+            break;
+            case '2': $scope.dos = "ganador.jpg";
+            break;
+            case '3': $scope.tres = "ganador.jpg";
+            break;
+            case '4': $scope.cuatro = "ganador.jpg";
+            break;
+            case '5': $scope.cinco = "ganador.jpg";
+            break;
+            case '6': $scope.seis = "ganador.jpg";
+        }
+    }
+
+
+
+    }
+
+    $scope.iniciarJuego();
+
+
+
+})
+
 .controller('loginCtrl',function($scope,$http,$auth){
 
     $scope.dato = {};
 
     $scope.login = function (){
 
-        $auth.login(
-
-            { usuario: $scope.dato.usuario,
-              clave: $scope.dato.clave}
-        )
-
-        .then(
-            function(rta){
-                console.log("RTA", rta);
-            if( $auth.isAuthenticated() )
-                console.info("info login:",$auth.getPayload());
-            },
-            function(error){
-                console.log("ERROR",error);
+        $auth.login({
+            usuario: $scope.datos.usuario,
+            clave: $scope.datos.clave
+        })
+        .then(function(response){
+            if($auth.isAuthenticated()){
+                //Si se logueó correctamente, isAuthenticated vale true. Entonces muestro por consola y redirijo al ABM.
+                console.log("Sesión iniciada!");
+                console.info("Info isAuthenticated: ", $auth.isAuthenticated());
+                console.info("Info getPayload: ", $auth.getPayload());
+                console.info("Info response: ", response);
+                $state.go('abm.alta');
             }
-        )
+            else{
+                console.info("Info isAuthenticated: ", $auth.isAuthenticated());
+                console.info("info no-login: ", $auth.getPayload());
+            }
+        },
+        function(err){
+            console.log("Error de conexión", err);
+        });
 
     }
 
